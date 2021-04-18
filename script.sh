cddb1d2e0c413c63bbbbb06a164c8be35c64a8a0
psumesh <spumesh@outlook.com>
2021-04-18 18:38:45 +0530

    commit_message

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "commit_message : "
+read commit_message
+
+echo "username : "
+read username
+
+echo "password : "
+read password
+
+git status
+git add .
+git commit -m commit_message
+git push https://github.com/psumesh/SPI_to_I2C_bridge.git master
+username
+password
